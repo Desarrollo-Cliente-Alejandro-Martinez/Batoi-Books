e47d399f2228fc5cc17656456f9b3f3da67eeb59
ALEXl0CO <alexadrialvaro@gmail.com>
2024-10-04 17:18:39 +0200

    Creación y preparación del proyecto.

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+import logoBatoi from '/logoBatoi.png'
+
+document.querySelector('#app').innerHTML = `
+  <div class="contenedor">
+    <img src="${logoBatoi}" class="logo" alt="Logo batoi"/>
+    <h1>BatoiBooks</h1>
+    <p>Abre la consola para ver el resutlado por favor</p>
+  </div>
+`
